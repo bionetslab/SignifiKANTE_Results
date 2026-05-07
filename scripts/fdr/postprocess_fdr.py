80fdf8bc1dc7ed3795179bfc049a26f8f1c20be0
Fabian Woller <iwbn106h@woody4.nhr.fau.de>
2026-05-07 12:53:15 +0200

    Add postprocessing scripts.

---
+++ scripts/fdr/postprocess_fdr.py
@@ -0,0 +1,308 @@
+import pandas as pd
+import os
+from sklearn.metrics import mean_absolute_error, f1_score, precision_score, recall_score
+from scipy.stats import pearsonr
+import pickle
+from statsmodels.stats.multitest import multipletests
+from sklearn.metrics import confusion_matrix
+
+def aggregate_groundtruth_results(root_dir : str,
+                                  tissue_list : list,
+                                  groundtruth_name : str
+                                  ):
+    # Iterate over selected tissues in root directory.
+    for subdir in os.listdir(root_dir):
+        subdir_path = os.path.join(root_dir, subdir)
+    
+        # Check if it's a directory
+        if os.path.isdir(subdir_path) and subdir in tissue_list:
+            print("Processing tissue ", subdir)
+            
+            batch_dir = os.path.join(subdir_path, groundtruth_name)
+        
+            # Navigate into FDR controlled batches.
+            if os.path.isdir(batch_dir):
+                #print(f"Processing directory: {batch_dir}")
+
+                # Iterate over all .csv files inside 'batch_wise_fdr_grn'
+                all_dfs = []
+                for file in os.listdir(batch_dir):
+                    if file.startswith("fdr"):
+                        file_path = os.path.join(batch_dir, file)
+                        #print(f"  Reading file: {file_path}")
+
+                        # Read CSV into DataFrame
+                        df = pd.read_csv(file_path)
+                        all_dfs.append(df)
+            
+                # Concatenate all dataframes.
+                combined_df = pd.concat(all_dfs, ignore_index=True)
+                                
+                combined_df_path = os.path.join(batch_dir, "groundtruth_grn.csv")
+                combined_df.to_csv(combined_df_path, index=False)
+            
+                # Iterate over all runtimes txt files and compute total runtime.
+                #total_runtime = 0.0
+                #for file in os.listdir(batch_dir):
+                #    if file.startswith("time"):
+                #        time_file = os.path.join(batch_dir, file)
+                #        #print(f"  Reading file: {time_file}")
+                #        # Read runtime from batch file.
+                #        with open(time_file, "r") as f:
+                #            runtime = float(f.read().strip())
+                #            total_runtime += runtime
+                #runtime_out_file = os.path.join(batch_dir, "groundtruth_runtime.txt")
+                #with open(runtime_out_file, "w") as f:
+                #    f.write(f'{total_runtime}\n')
+            
+                # Iterate over all emissions csv files and compute total emission.
+                #total_emissions = 0.0
+                #for file in os.listdir(batch_dir):
+                #    if file.startswith("emissions"):
+                #        em_file = os.path.join(batch_dir, file)
+                #        print(f"  Reading file: {em_file}")
+                #        # Read runtime from batch file.
+                #        em_df = pd.read_csv(em_file)
+                #        if len(em_df) > 1:
+                #            print("Warning: emissions df has more than one row!")
+                #        total_emissions += em_df['emissions'][0]
+                #em_file_out = os.path.join(batch_dir, "groundtruth_emissions.txt")
+                #with open(em_file_out, "w") as f:
+                #    f.write(f'{total_emissions}\n')
+
+def compute_approx_fdr_metrics(root_dir : str,
+                               tissue_list : list,
+                               non_tf_list : list,
+                               tf_list : list,
+                               approx_dir_name : str,
+                               groundtruth_dir : str,
+                               adjust_pvalues : bool,
+                               num_permutations : int = 1000,
+                               apply_benjamini_hb : bool = False):
+    all_tissues_dict = dict()
+    for subdir in os.listdir(root_dir):
+        subdir_path = os.path.join(root_dir, subdir)
+    
+        # Check if it's a directory
+        if os.path.isdir(subdir_path) and subdir in tissue_list:
+            print("Processing tissue ", subdir)
+            approx_dir = os.path.join(subdir_path, approx_dir_name)
+        
+            # Navigate into FDR controlled approximation.
+            if os.path.isdir(approx_dir):
+                
+                # Iterate over all cluster sizes.
+                tissue_dict = dict()
+                for num_tfs in tf_list:
+                    for num_non_tfs in non_tf_list:
+                        
+                        fdr_file_name = f'fdr_grn_nontf_{num_non_tfs}_numtf_{num_tfs}.csv'
+                        time_file_name = f'time_nontf_{num_non_tfs}_numtf_{num_tfs}.txt'
+                        em_file_name = f'emissions_nontf_{num_non_tfs}_numtf_{num_tfs}.csv'
+                        fdr_path = os.path.join(approx_dir, fdr_file_name)
+                        time_path = os.path.join(approx_dir, time_file_name)
+                        em_path = os.path.join(approx_dir, em_file_name)
+                        
+                        # Compute F1, Precision, Recall on approx. FDR controlled GRNs.
+                        approx_grn = pd.read_csv(fdr_path)
+                        gt_path = os.path.join(subdir_path, groundtruth_dir, "groundtruth_grn.csv")
+                        gt_grn = pd.read_csv(gt_path)
+                        #approx_grn = approx_grn.sort_values(by='importance', ascending=False)
+                        #gt_grn = gt_grn.sort_values(by='importance', ascending=False)
+                        
+                        if apply_benjamini_hb:
+                            _, pvals_corrected_approx, _, _ = multipletests(approx_grn['pvalue'], method='fdr_bh')
+                            _, pvals_corrected_gt, _, _ = multipletests(gt_grn['pvalue'], method='fdr_bh')
+                            approx_grn.drop(columns=['pvalue'], inplace=True)
+                            gt_grn.drop(columns=['pvalue'], inplace=True)
+                            approx_grn['pvalue'] = pvals_corrected_approx
+                            gt_grn['pvalue'] = pvals_corrected_gt
+                        
+                        if adjust_pvalues:
+                            gt_max_occurence = max(gt_grn['shuffled_occurences'])
+                            approx_max_occurence = max(approx_grn['shuffled_occurences'])
+                            gt_grn['pvalue_adj'] = gt_grn['pvalue'] * (gt_max_occurence+1.0) / (gt_grn['shuffled_occurences']+1.0)
+                            approx_grn['pvalue_adj'] = approx_grn['pvalue'] * (approx_max_occurence+1.0) / (approx_grn['shuffled_occurences']+1.0)
+                            gt_grn.drop(columns=['pvalue'], inplace=True)
+                            approx_grn.drop(columns=['pvalue'], inplace=True)
+                            gt_grn.rename(columns={"pvalue_adj" : 'pvalue'}, inplace=True)
+                            approx_grn.rename(columns={"pvalue_adj" : 'pvalue'}, inplace=True)
+                            
+                        
+                        merged_grn = pd.merge(approx_grn, gt_grn, on=['TF', 'target'], how='inner', suffixes=('_approx', '_gt'))
+                        # Sort GRNs by importance.
+                        approx_pvals = list(merged_grn['pvalue_approx'])
+                        gt_pvals = list(merged_grn['pvalue_gt'])
+                        mae = mean_absolute_error(gt_pvals, approx_pvals)
+                        corr = pearsonr(gt_pvals, approx_pvals)[0]
+                        # Threshold pvalues at 005 and 001.
+                        approx_005 = [1 if pval <= 0.05 else 0 for pval in approx_pvals]
+                        approx_001 = [1 if pval <= 0.01 else 0 for pval in approx_pvals]
+                        gt_005 = [1 if pval <= 0.05 else 0 for pval in gt_pvals]
+                        gt_001 = [1 if pval <= 0.01 else 0 for pval in gt_pvals]
+                        f1_005 = f1_score(gt_005, approx_005)
+                        f1_001 = f1_score(gt_001, approx_001)
+                        
+                        tn, fp, fn, tp = confusion_matrix(gt_001, approx_001, labels=[0,1]).ravel()
+                        
+                        # Compute runtime savings.
+                        with open(time_path, "r") as f:
+                            approx_runtime = float(f.read().strip())
+                        gt_time_path = os.path.join(subdir_path, groundtruth_dir, "groundtruth_runtime.txt")
+                        with open(gt_time_path, "r") as f:
+                            gt_time = float(f.read().strip())
+                        absolute_time_saving = gt_time - approx_runtime
+                        time_savings_factor = gt_time / approx_runtime
+                        
+                        # Compute emissions savings.
+                        em_df = pd.read_csv(em_path)
+                        if len(em_df) > 1:
+                            print("Warning: emissions df has more than one row!")
+                        approx_em = em_df['emissions'][0]
+                        gt_em_path = os.path.join(subdir_path, groundtruth_dir, "groundtruth_emissions.txt")
+                        with open(gt_em_path, "r") as f:
+                            gt_em = float(f.read().strip())
+                        absolute_em_saving = gt_em - approx_em
+                        em_savings_factor = gt_em / approx_em
+                        
+                        # Save results.
+                        tissue_dict[(num_non_tfs, num_tfs)] = {'mae': mae, 
+                                                               'f1_005': f1_005, 
+                                                               'f1_001': f1_001,
+                                                               'abs_time_saving': absolute_time_saving,
+                                                               'factor_time_saving': time_savings_factor,
+                                                               'abs_emission_saving': absolute_em_saving,
+                                                               'factor_emission_saving': em_savings_factor,
+                                                               'total_runtime' : approx_runtime,
+                                                               'correlation' : corr,
+                                                               'tp_001' : tp
+                                                               }
+                
+                tissue_res_file = os.path.join(approx_dir, "approx_fdr_results.pkl")
+                with open(tissue_res_file, 'wb') as f:
+                    pickle.dump(tissue_dict, f)
+                all_tissues_dict[subdir] = tissue_dict
+    return all_tissues_dict
+
+def pickle_results_to_df(root_dir : str,
+                        tissue_list : list,
+                        approx_dir_name : str,
+                        out_file_name : str
+                        ):
+    # Iterate over selected tissues in root directory.
+    all_results_dict = {
+        'tissue': [],
+        'num_non_tfs': [],
+        'num_tfs': [],
+        'mae': [],
+        'f1_005': [],
+        'f1_001': [],
+        'abs_time_saving': [],
+        'rel_time_saving': [],
+        'abs_emission_saving': [],
+        'rel_emission_saving': [],
+        'total_runtime': [],
+        'correlation':[],
+        'tp_001':[]
+    }
+    
+    for subdir in os.listdir(root_dir):
+        subdir_path = os.path.join(root_dir, subdir)
+    
+        # Check if it's a directory
+        if os.path.isdir(subdir_path) and subdir in tissue_list:
+            print("Processing tissue ", subdir)
+            approx_dir = os.path.join(subdir_path, approx_dir_name)
+        
+            # Open aggregated pickle results file.
+            pickle_file = os.path.join(approx_dir, "approx_fdr_results.pkl")
+            with open(pickle_file, "rb") as f:
+                res_dict = pickle.load(f)
+            tissue = subdir
+            # Iterate through results dict and add entries to df.
+            for key, val in res_dict.items():
+                num_non_tfs = key[0]
+                num_tfs = key[1]
+                mae = val['mae']
+                f1_005 = val['f1_005']
+                f1_001 = val['f1_001']
+                abs_time = val['abs_time_saving']
+                rel_time = val['factor_time_saving']
+                abs_em = val['abs_emission_saving']
+                rel_em = val['factor_emission_saving']
+                total_time = val['total_runtime']
+                correlation = val['correlation']
+                tp_001 = val['tp_001']
+                all_results_dict['tissue'].append(tissue)
+                all_results_dict['num_non_tfs'].append(num_non_tfs)
+                all_results_dict['num_tfs'].append(num_tfs)
+                all_results_dict['mae'].append(mae)
+                all_results_dict['f1_005'].append(f1_005)
+                all_results_dict['f1_001'].append(f1_001)
+                all_results_dict['abs_time_saving'].append(abs_time)
+                all_results_dict['rel_time_saving'].append(rel_time)
+                all_results_dict['abs_emission_saving'].append(abs_em)
+                all_results_dict['rel_emission_saving'].append(rel_em)
+                all_results_dict['total_runtime'].append(total_time)
+                all_results_dict['correlation'].append(correlation)
+                all_results_dict['tp_001'].append(tp_001)
+    
+    all_res_df = pd.DataFrame(all_results_dict)
+    all_res_df.to_csv(f'./{out_file_name}', index=False)
+
+
+if __name__ == "__main__":
+    root_dir = "/home/woody/iwbn/iwbn106h/gtex_fdr_results/"
+
+    full_tissue_list = [
+    "Adipose_Tissue", "Adrenal_Gland", "Bladder", "Blood", "Blood_Vessel",
+    "Brain", "Breast", "Cervix_Uteri", "Colon", "Esophagus",
+    "Fallopian_Tube", "Heart", "Kidney", "Liver", "Lung",
+    "Muscle", "Nerve", "Ovary", "Pancreas", "Pituitary",
+    "Prostate", "Salivary_Gland", "Skin", "Small_Intestine", "Spleen",
+    "Stomach", "Testis", "Thyroid", "Uterus", "Vagina"
+    ]
+    
+    sub_tissue_list = ["Breast", "Kidney", "Testis"]
+    root_dir = "gtex_fdr_results"
+    groundtruth_name = ""
+
+    ### Version 1 ###
+    # Code for aggreagting groundtruth results - only needs to be done once.
+    #aggregate_groundtruth_results(root_dir,
+    #                              sub_tissue_list,
+    #                              groundtruth_name)
+    #print("Aggregated GT results!")
+    #quit()
+
+    ### Version 2 ###
+    # Approx. FDR results computation.
+    non_tf_list = list(range(1, 10, 1)) + list(range(10, 101, 10))
+    #non_tf_list = [3,4,5,6,7,8,9,10,20,30,40,50,70,100]
+    tf_list = [-1]
+    # From which results directory in respective tissue directory to take approximate P-value results.
+    #approx_dir_name = "approximate_fdr_grns_random"
+    approx_dir_name = "random_targets_wasserstein_with_shuffled_occurences"
+    # From which directory in respective tissue directory to take groundtruth P-value results from.
+    #groundtruth_dir = "batch_wise_fdr_grns"
+    groundtruth_dir = "groundtruth_with_shuffled_occurences"
+    # Whether or not scale empirical counts for number of actual occurences of edge in permuted GRN.
+    adjust_pvalues = True 
+    # Whether or not to apply BH-correction on both groundtruth and approximate P-values.
+    use_bh = False
+    res = compute_approx_fdr_metrics(root_dir,
+                                     sub_tissue_list,
+                                     non_tf_list, 
+                                     tf_list,
+                                     approx_dir_name,
+                                     groundtruth_dir, 
+                                     adjust_pvalues,
+                                     apply_benjamini_hb=use_bh)
+    
+    # Name of results file.
+    out_file_name = "random_targets_wasserstein_no-benjamini_occurence-adjusted.csv"
+    pickle_results_to_df(root_dir,
+                         sub_tissue_list,
+                         approx_dir_name,
+                         out_file_name)
+    
