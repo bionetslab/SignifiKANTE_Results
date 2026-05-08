e1b84c8826fdcaf533fe86979f15301e8f144dc9
Paul Martini <iwbn107h@woody4.nhr.fau.de>
2026-05-08 09:24:10 +0200

    set number of requested cpus to 30

--- scripts/sc_example/array_job_approx.sh
+++ scripts/sc_example/array_job_approx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=30
 #SBATCH --time=24:00:00
 #SBATCH --export=NONE
 #SBATCH --array=1-570%570
