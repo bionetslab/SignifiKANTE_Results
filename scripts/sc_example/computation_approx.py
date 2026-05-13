eaf6269defeddf98b3f07850843c83543012d944
Paul Martini <paul.martini@web.de>
2026-05-13 14:37:13 +0200

    define client explicitly

--- scripts/sc_example/computation_approx.py
+++ scripts/sc_example/computation_approx.py
@@ -5,6 +5,7 @@ import argparse
 import pandas as pd
 
 from typing import Dict
+from distributed import LocalCluster, Client
 from codecarbon import OfflineEmissionsTracker
 from tracking import scalability_wrapper
 from signifikante.algo import signifikante_fdr
@@ -28,6 +29,20 @@ def compute_approx_fdr(config: Dict):
     expression_mat = pd.read_csv(data_path)
     grn_input = pd.read_csv(grn_path)
 
+    n_cpus = int(os.environ.get('SLURM_CPUS_PER_TASK', 1))
+    n_workers = min(n_cpus, num_clusters)
+    local_cluster = LocalCluster(
+        n_workers=n_workers,
+        threads_per_worker=1,
+        processes=True,
+        diagnostics_port=None,
+    )
+    client = Client(local_cluster)
+
+    print(client)
+    print(client.ncores())
+    print(f'Number of workers: {len(client.scheduler_info()["workers"])}')
+
     def run():
 
         grn_fdr = signifikante_fdr(
@@ -39,7 +54,7 @@ def compute_approx_fdr(config: Dict):
             input_grn=grn_input,
             target_subset=None,
             tf_names=None,
-            client_or_address='local',
+            client_or_address=client,
             seed=42 + num_clusters,
             verbose=False,
             num_permutations=k,
@@ -48,50 +63,58 @@ def compute_approx_fdr(config: Dict):
             inference_mode='grnboost2',
             apply_bh_correction=True,
             normalize_gene_expression=False,
+            apply_westfall_young=True,
+            return_cluster_ids=True,
         )
 
         return grn_fdr
 
     fn_emissions = os.path.join(
         result_dir,
-        f'emissions_{dataset_name}_num_clust_{num_clusters:03d}_num_permut_{k:05d}_grn_id_{grn_id:02d}.csv'
+        f'emissions_{dataset_name}_num_clust_{num_clusters:03d}_num_permut_{k:04d}_grn_id_{grn_id:02d}.csv'
     )
 
-    with OfflineEmissionsTracker(
-            country_iso_code='DEU', output_file=fn_emissions, log_level='error', measure_power_secs=600
-    ) as tracker:
+    try:
+        with OfflineEmissionsTracker(
+                country_iso_code='DEU', output_file=fn_emissions, log_level='error', measure_power_secs=600
+        ) as tracker:
 
-        wall_time, mem_samples, grn = scalability_wrapper(
-            function=run, function_params=None, tracking_interval=0.1
-        )
+            wall_time, mem_samples, grn = scalability_wrapper(
+                function=run, function_params=None, tracking_interval=0.1
+            )
 
-        grn.to_csv(
-            os.path.join(
-                result_dir,
-                f'grn_{dataset_name}_num_clust_{num_clusters:03d}_num_permut_{k:05d}_grn_id_{grn_id:02d}.csv'
-            ),
-            index=False
-        )
+            grn.to_csv(
+                os.path.join(
+                    result_dir,
+                    f'grn_{dataset_name}_num_clust_{num_clusters:03d}_num_permut_{k:04d}_grn_id_{grn_id:02d}.csv'
+                ),
+                index=False
+            )
 
-        mem_peak = max(mem_samples)
-        num_samples = len(mem_samples)
-        mem_avg = sum(mem_samples) / num_samples
+            mem_peak = max(mem_samples)
+            num_samples = len(mem_samples)
+            mem_avg = sum(mem_samples) / num_samples
 
-        tracking_df = pd.DataFrame([{
-            'dataset': dataset_name,
-            'num_clusters': num_clusters,
-            'wall_time': wall_time,
-            'mem_peak': mem_peak,
-            'mem_avg': mem_avg,
-            'num_samples': num_samples,
-        }])
-        tracking_df.to_csv(
-            os.path.join(
-                result_dir,
-                f'tracking_{dataset_name}_num_clust_{num_clusters:03d}_num_permut_{k:05d}_grn_id_{grn_id:02d}.csv'
-            ),
-            index=False
-        )
+            tracking_df = pd.DataFrame([{
+                'dataset': dataset_name,
+                'num_clusters': num_clusters,
+                'num_permutations': k,
+                'wall_time': wall_time,
+                'mem_peak': mem_peak,
+                'mem_avg': mem_avg,
+                'num_samples': num_samples,
+            }])
+            tracking_df.to_csv(
+                os.path.join(
+                    result_dir,
+                    f'tracking_{dataset_name}_num_clust_{num_clusters:03d}_num_permut_{k:05d}_grn_id_{grn_id:02d}.csv'
+                ),
+                index=False
+            )
+
+    finally:
+        client.close()
+        local_cluster.close()
 
 
 if __name__ == '__main__':
--- scripts/sc_example/computation_ground_truth.py
+++ scripts/sc_example/computation_ground_truth.py
@@ -5,6 +5,7 @@ import argparse
 import pandas as pd
 
 from typing import Dict
+from distributed import LocalCluster, Client
 from codecarbon import OfflineEmissionsTracker
 from tracking import scalability_wrapper
 from signifikante.algo import signifikante_fdr
@@ -17,6 +18,7 @@ def compute_classical_fdr(config: Dict):
 
     dataset_name = config['dataset_name']
     run_id = config['run_id']
+    k = config['num_permutations']
     data_path = config['data_path']
     grn_path = config['grn_path']
     result_dir = config['result_dir']
@@ -25,6 +27,19 @@ def compute_classical_fdr(config: Dict):
 
     expression_mat = pd.read_csv(data_path)
     grn_input = pd.read_csv(grn_path)
+
+    n_cpus = int(os.environ.get('SLURM_CPUS_PER_TASK', 1))
+    local_cluster = LocalCluster(
+        n_workers=n_cpus,
+        threads_per_worker=1,
+        processes=True,
+        diagnostics_port=None,
+    )
+    client = Client(local_cluster)
+
+    print(client)
+    print(client.ncores())
+    print(f'Number of workers: {len(client.scheduler_info()["workers"])}')
 
     def run():
         grn_fdr = signifikante_fdr(
@@ -35,44 +50,50 @@ def compute_classical_fdr(config: Dict):
             input_grn=grn_input,
             tf_names=None,
             target_subset=None,
-            client_or_address='local',
+            client_or_address=client,
             seed=42 + run_id,
             verbose=False,
-            num_permutations=1000,
+            num_permutations=k,
             output_dir=None,
             scale_for_tf_sampling=True,
             inference_mode='grnboost2',
             apply_bh_correction=True,
             normalize_gene_expression=False,
         )
-
         return grn_fdr
 
-    fn_emissions = os.path.join(result_dir, f'emissions_{dataset_name}_{run_id:02d}.csv')
+    fn_emissions = os.path.join(result_dir, f'emissions_{dataset_name}_num_permut_{k:04d}_{run_id:02d}.csv')
 
-    with OfflineEmissionsTracker(
-            country_iso_code='DEU', output_file=fn_emissions, log_level='error', measure_power_secs=600
-    ) as tracker:
+    try:
 
-        wall_time, mem_samples, grn = scalability_wrapper(
-            function=run, function_params=None, tracking_interval=0.1
-        )
+        with OfflineEmissionsTracker(
+                country_iso_code='DEU', output_file=fn_emissions, log_level='error', measure_power_secs=600
+        ) as tracker:
 
-        grn.to_csv(os.path.join(result_dir, f'grn_{dataset_name}_{run_id:02d}.csv'), index=False)
+            wall_time, mem_samples, grn = scalability_wrapper(
+                function=run, function_params=None, tracking_interval=0.1
+            )
 
-        mem_peak = max(mem_samples)
-        num_samples = len(mem_samples)
-        mem_avg = sum(mem_samples) / num_samples
+            grn.to_csv(os.path.join(result_dir, f'grn_{dataset_name}_num_permut_{k:04d}_{run_id:02d}.csv'), index=False)
 
-        tracking_df = pd.DataFrame([{
-            'dataset': dataset_name,
-            'run_id': run_id,
-            'wall_time': wall_time,
-            'mem_peak': mem_peak,
-            'mem_avg': mem_avg,
-            'num_samples': num_samples,
-        }])
-        tracking_df.to_csv(os.path.join(result_dir, f'tracking_{dataset_name}_{run_id:02d}.csv'), index=False)
+            mem_peak = max(mem_samples)
+            num_samples = len(mem_samples)
+            mem_avg = sum(mem_samples) / num_samples
+
+            tracking_df = pd.DataFrame([{
+                'dataset': dataset_name,
+                'run_id': run_id,
+                'num_permutations': k,
+                'wall_time': wall_time,
+                'mem_peak': mem_peak,
+                'mem_avg': mem_avg,
+                'num_samples': num_samples,
+            }])
+            tracking_df.to_csv(os.path.join(result_dir, f'tracking_{dataset_name}_num_permut_{k:04d}_{run_id:02d}.csv'), index=False)
+
+    finally:
+        client.close()
+        local_cluster.close()
 
 
 if __name__ == '__main__':
